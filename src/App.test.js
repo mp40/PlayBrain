932f191d47d6d9eecaf43b1a30b8ed0bae942d6b
Matthew Philps <philps.matthew@gmail.com>
2019-02-21 10:52:40 +0900

    basic file setup

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {mount} from 'enzyme'
+
+describe('rendering components',()=>{
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('should not render the Modal component on start',()=>{
+    const wrapper = mount(<App/>)
+    expect(wrapper.text()).not.toContain('Modal displayed')
+  })
+})
+
+describe('the modal button',()=>{
+  it('should open the modal when button is clicked',()=>{
+    const wrapper = mount(<App/>)
+    wrapper.find('#openModal').simulate('click')
+    expect(wrapper.text()).toContain('Modal displayed')
+  })
+})
