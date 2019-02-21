4ec390226ee9790efdcdd8ad39da0e4211d410d6
Matthew Philps <philps.matthew@gmail.com>
2019-02-21 15:16:06 +0900

    failing test written for filterByRegion

--- src/helperFunctions.spec.js
+++ src/helperFunctions.spec.js
@@ -1,7 +1,11 @@
 const filterByRegion = require('./helperFunctions')
+const playersMock = require('./playersMock')
 
 describe('filtering players by region',()=>{
     it('should return an array',()=>{
         expect(Array.isArray(filterByRegion())).toEqual(true)
+    })
+    it('should return all the Japanese players if parameter is "jp"',()=>{
+        expect(filterByRegion(playersMock).length).toBe(15)
     })
 })
