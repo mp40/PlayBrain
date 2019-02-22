0f908b8a888d433910678ea16644e3603950e576
Matthew Philps <philps.matthew@gmail.com>
2019-02-22 12:31:12 +0900

    refactor

--- src/App.test.js
+++ src/App.test.js
@@ -160,6 +160,7 @@ describe('The Resuts component',()=>{
   })
 })
 
+
 describe('admin',()=>{
   describe('the results page', ()=>{
     it('should render the results page when admin closes votation',()=>{
